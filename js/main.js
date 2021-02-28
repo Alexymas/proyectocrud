4afabd1d76c45eaf1f4b81f1c5291492c58754ce
Alexymas <76096182+Alexymas@users.noreply.github.com>
2021-02-27 20:09:38 -0700

    Cambiado de repositorio, se eliminara el anterior

---
+++ js/main.js
@@ -0,0 +1,162 @@
+import productDb, {
+    bulkcreate,
+    getData,
+    createElement
+} from './Module.js'
+
+
+let db = productDb("Productdb", {
+    products : '++id, name, seller, price'
+} );
+
+//Input Tags
+const userid = document.getElementById("userid");
+const proname = document.getElementById("proname");
+const seller = document.getElementById("seller");
+const price = document.getElementById("price");
+
+// buttons
+const btncreate = document.getElementById("btn-create");
+const btnread = document.getElementById("btn-read");
+const btnuptdate = document.getElementById("btn-update");
+const btndelete = document.getElementById("btn-delete");
+
+//not found
+const notfound = document.getElementById("notfound");
+
+//Insert value using create button
+btncreate.onclick=(event)=>{
+    let flag = bulkcreate(db.products, {
+        name: proname.value,
+        seller : seller.value,
+        price : price.value,
+    })
+    //console.log(flag);
+
+    /*proname.value="";
+    seller.value="";
+    price.value=""*/
+
+    proname.value = seller.value = price.value = "";
+    getData(db.products, (data) =>{
+        userid.value = data.id + 1 || 1;
+        
+    });
+
+    table();
+    let insertmsg = document.querySelector(".insertmsg");
+    getMsg(flag, insertmsg);
+
+
+};
+
+//Create event on read button
+btnread.onclick = table;
+
+// Create event on Update button
+btnuptdate.onclick = ()=>{
+    const id = parseInt(userid.value || 0)
+    if(id){
+        db.products.update(id, {
+            name : proname.value,
+            seller : seller.value,
+            price : price.value
+        }).then((updated) =>{
+            //let get = updated?`data Updated`:`Couldn´t Update Data`;
+            let get = updated ? true:false;
+            let updatemsg=document.querySelector(".updatemsg");
+            getMsg(get, updatemsg);
+
+            proname.value = seller.value = price.value="";
+        })
+    }
+}
+
+//Delete all records
+btndelete.onclick =()=>{
+    db.delete();
+    db = productDb("Productdb", {
+        products : '++id, name, seller, price'
+    } );
+    db.open();
+    table()
+    textID(userid)
+
+    let deletemsg = document.querySelector(".deletemsg");
+    getMsg (true, deletemsg)
+}
+
+//Window onload event
+window.onload = () =>{
+    textID(userid)
+}
+function textID(textboxid){
+    getData(db.products, data=>{
+        textboxid.value = data.id + 1|| 1
+    })
+}
+
+function table(){
+    const tbody = document.getElementById("tbody");
+    while(tbody.hasChildNodes()){
+        tbody.removeChild(tbody.firstChild);
+    };
+
+    getData(db.products, (data)=>{
+        if(data){
+            createElement("tr", tbody, tr=>{
+                for (const value in data) {
+                    createElement("td", tr, td=>{
+                        td.textContent = data.price === data[value] ? `$ ${data[value]}`: data[value];
+                    });
+                };
+                createElement("td", tr, td=>{
+                    createElement("i", td, i =>{
+                        i.className +="fas fa-edit btnedit"
+                        i.setAttribute(`data-id`, data.id);
+                        i.onclick = editbtn;
+                    });
+                });
+                createElement("td", tr, td=>{
+                    createElement("i", td, i =>{
+                        i.className +="fas fa-trash btndelete"
+                        i.setAttribute(`data-id`, data.id);
+                        i.onclick = deletebtn;
+                    });
+                });
+            });
+        }else{
+            notfound.textContent = "No se encontro registro en la base de datos"
+
+        }
+    });
+};
+
+function editbtn(event){
+    let id = parseInt(event.target.dataset.id);
+
+    db.products.get(id, data =>{
+        userid.value = data.id || 0;
+        proname.value = data.name || "";
+        seller.value = data.seller || "";
+        price.value = data.price || "";
+    });
+};
+
+function deletebtn(event){
+    let id = parseInt(event.target.dataset.id);
+    db.products.delete(id);
+    table();
+};
+
+function getMsg(flag, element){
+    if(flag){
+        element.className += " movedown"; //note the space between the "" and movedown
+        
+        setTimeout(()=>{
+            element.classList.forEach(classname => {
+                classname == "movedown"?undefined : element.classList.remove("movedown")
+            });
+        }, 4000)
+    }
+}
